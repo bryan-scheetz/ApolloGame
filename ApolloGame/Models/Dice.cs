1256e6d0a26856be5f8aa818753d5149b0fac917
bryan-scheetz <70301389+bryan-scheetz@users.noreply.github.com>
2020-10-20 21:28:35 -0400

    1. Completed view of damage counter; Created Dice model

---
+++ ApolloGame/Models/Dice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApolloGame.Models
+{
+    public class Dice
+    {
+        public int currentFace { get; set; }
+
+
+        public void Roll()
+        {
+            var rand = new Random();
+            this.currentFace = rand.Next(7);
+        }
+
+    }
+}
