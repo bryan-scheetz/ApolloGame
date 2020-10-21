90ccc4e44ac2d4502ca323611363ce305a17fc15
bryan-scheetz <70301389+bryan-scheetz@users.noreply.github.com>
2020-10-20 22:05:13 -0400

    Added dice control

--- ApolloGame/Controllers/DiceController.cs
+++ ApolloGame/Controllers/DiceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace ApolloGame.Controllers
 {
     public class DiceController
     {
+        public void Roll()
+        {
+            
+        }
     }
 }
--- ApolloGame/ViewModel/DamageCounterViewModel.cs
+++ ApolloGame/ViewModel/DamageCounterViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace ApolloGame.ViewModel
 {
     public class DamageCounterViewModel
